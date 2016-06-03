aa5818b43806514127fb1457148715fa178e8e82
Daredos <asiosldh@163.com>
2016-06-04 00:18:33 +0800

    first commit

---
+++ LDTour/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LDTour
+//
+//  Created by Daredos on 16/6/4.
+//  Copyright © 2016年 Daredos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LDTour/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LDTour
+//
+//  Created by Daredos on 16/6/4.
+//  Copyright © 2016年 Daredos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
