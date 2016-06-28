cf466704b2c66ea45c15cda97f3c8f69be773ae0
梁大红 <liangdahong@bluemoon.com.cn>
2016-06-28 17:08:46 +0800

    add knowledge

---
+++ LDTour/LDUtil/knowledge.h
@@ -0,0 +1,13 @@
+//
+//  knowledge.h
+//  LDTour
+//
+//  Created by Daredos on 16/6/28.
+//  Copyright © 2016年 Daredos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface knowledge : NSObject
+
+@end
---
+++ LDTour/LDUtil/knowledge.m
@@ -0,0 +1,13 @@
+//
+//  knowledge.m
+//  LDTour
+//
+//  Created by Daredos on 16/6/28.
+//  Copyright © 2016年 Daredos. All rights reserved.
+//
+
+#import "knowledge.h"
+
+@implementation knowledge
+
+@end
